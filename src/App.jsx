6f5ed534f70e94bf52574a93bc411290ed466a7a
Hajarah <mannehajaratou@gmail.com>
2025-12-16 20:48:50 +0000

    Initial frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Dashboard from "./pages/Dashboard"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <header className="bg-white shadow p-4">
+        <h1 className="text-xl font-bold">
+          Smart Security System
+        </h1>
+      </header>
+
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
